e6f4828bad5e6b949e722b86542af9720b9ae6aa
LionelMv <mvangicode@gmail.com>
2023-05-23 20:23:41 +0300

    additional comments

--- console.py
+++ console.py
@@ -40,9 +40,9 @@ class HBNBCommand(cmd.Cmd):
         if line == "" or line is None:
             print("** class name missing **")
         else:
-            args = line.split(' ')
+            args = line.split()
             objdict = storage.all()
-            if args[0] not in HBNBCommand.classes:
+            if args[0] not in HBNBCommand.__valid_classes:
                 print("** class doesn't exist **")
             elif len(args) < 2:
                 print("** instance id missing **")
@@ -59,7 +59,7 @@ class HBNBCommand(cmd.Cmd):
         if line == "" or line is None:
             print("** class name missing **")
         else:
-            args = line.split(' ')
+            args = line.split()
             objdict = storage.all()
             if args[0] not in HBNBCommand.classes:
                 print("** class doesn't exist **")
@@ -72,6 +72,46 @@ class HBNBCommand(cmd.Cmd):
                 else:
                     del objdict[key]
                     storage.save()
+
+    def do_all(self, line):
+        """Prints string representation of all instances.
+        """
+        # do_all(BaseModel)
+        if line != "":
+            args = line.split()
+            if args[0] not in HBNBCommand.classes:
+                print("** class doesn't exist **")
+            else:
+                nl = [str(obj) for key, obj in storage.all().items()
+                      # if __class__.__name__ == args[0]
+                      if type(obj).__name__ == args[0]
+                      # if obj.__class__.__name__ == args[0]
+                print(nl)
+        else:
+            new_list = [str(obj) for key, obj in storage.all().items()]
+            print(new_list)
+
+    def do_update(self, line):
+        """Update if given exact object, exact attribute"""
+        objdict = storage.all()
+        args = line.split()
+        if len(args) >= 4:
+            key = "{}.{}".format(args[0], args[1])
+            cast = type(eval(args[3]))
+            # setattr(objdict[key], args[2], cast(arg3))
+            objdict[key].save()
+        elif len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        elif ("{}.{}".format(args[0], args[1])) not in storage.all().keys():
+            print("** no instance found **")
+        elif len(args) == 2:
+            print("** attribute name missing **")
+        else:
+            print("** value missing **")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()

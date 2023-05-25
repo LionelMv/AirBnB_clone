6259f6c1a7944d8e903e3d9ec9d40906c14a96ef
LionelMv <mvangicode@gmail.com>
2023-05-26 00:09:36 +0300

    update if statements

--- console.py
+++ console.py
@@ -99,26 +99,28 @@ class HBNBCommand(cmd.Cmd):
     def do_update(self, line):
         """Update if given exact object, exact attribute"""
         args = line.split()
-        if len(args) >= 4:
-            key = f"{args[0]}.{args[1]}"
-            # Finding attribute type (casted).
-            arg_type = type(eval(args[3]))
-            arg3 = args[3].strip('"')
-            arg3 = arg3.strip("'")
-            setattr(storage.all()[key], args[2], arg_type(arg3))
-            storage.all()[key].save()
-        elif len(args) == 0:
+        if len(args) == 0:
             print("** class name missing **")
-        elif args[0] not in HBNBCommand.valid_classes:
-            print("** class doesn't exist **")
         elif len(args) == 1:
             print("** instance id missing **")
-        elif ("{}.{}".format(args[0], args[1])) not in storage.all().keys():
-            print("** no instance found **")
         elif len(args) == 2:
             print("** attribute name missing **")
-        else:
+        elif len(args) == 3:
             print("** value missing **")
+        else:
+            if args[0] not in HBNBCommand.valid_classes:
+                print("** class doesn't exist **")
+            elif ("{}.{}".format(args[0], args[1]))\
+                    not in storage.all().keys():
+                print("** no instance found **")
+            else:
+                key = f"{args[0]}.{args[1]}"
+                # Finding attribute type (casted).
+                arg_type = type(eval(args[3]))
+                arg3 = args[3].strip('"')
+                arg3 = arg3.strip("'")
+                setattr(storage.all()[key], args[2], arg_type(arg3))
+                storage.all()[key].save()
 
 
 if __name__ == '__main__':
